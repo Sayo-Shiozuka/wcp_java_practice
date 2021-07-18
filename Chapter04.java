db3055232a4515d4f4f602b83265b6ad1336e37c
Sayo Fukuyama <sayosf1026@gmail.com>
2021-07-18 18:58:14 +0900

    Chapter04.java

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    final int LUCKY_NUMBER = 7;
+    String str = "Sayo Shiozuka";
+    System.out.println(str);
+
+    str = "感謝";
+    System.out.println(str);
+    System.out.println(LUCKY_NUMBER);
+  }
+}
